1cbf2368eaa377010effa7797aa95187bd35f6bc
Anjeongkyun <dkswjdrbs12@naver.com>
2022-05-16 22:42:13 +0900

    Main 실행 로직 추가

--- core/src/main/java/hello/core/member/MemoryMemberRespository.java
+++ core/src/main/java/hello/core/member/MemoryMemberRespository.java
@@ -1,2 +1,20 @@
-package hello.core.member;public class MemoryMemberRespository {
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRespository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>();
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
 }
