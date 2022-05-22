55da62d309adbdabe0437a74fc3b8b2fd9d944b5
Anjeongkyun <dkswjdrbs12@naver.com>
2022-05-22 23:40:51 +0900

    ApplicationContext를 이용한 Bean 주입 및 메서드 매핑

--- core/src/main/java/hello/core/MemberApp.java
+++ core/src/main/java/hello/core/MemberApp.java
@@ -4,12 +4,20 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
 
-        MemberService memberService = appConfig.memberService();
+        //AppConfig에 있는 Bean들을 넣어주고 관리를 해준다.
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        //AppConfig에 있는 메서드 이름을 매핑
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
