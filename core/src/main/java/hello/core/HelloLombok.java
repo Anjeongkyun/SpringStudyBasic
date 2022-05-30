6655dc907b66db0228b30844d18de3b8ce37965a
Anjeongkyun <dkswjdrbs12@naver.com>
2022-05-30 23:16:40 +0900

    lombok 적용 후 테스트 코드 작성

--- core/src/main/java/hello/core/HelloLombok.java
+++ core/src/main/java/hello/core/HelloLombok.java
@@ -1,2 +1,25 @@
-package hello.core;public class HelloLombk {
+package hello.core;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class HelloLombok {
+
+    private String name;
+    private int age;
+
+    public static void main(String[] args) {
+        HelloLombok hl = new HelloLombok();
+
+        hl.setAge(27);
+        int myAge = hl.getAge();
+
+        System.out.println(myAge);
+        System.out.println(hl);
+
+    }
 }
